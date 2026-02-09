f07f01684f3fd1784dc9b730f9fbb995f54f0405
Mariana <mariana-batista99@hotmail.com>
2026-02-09 16:30:44 -0300

    adicionando novo curso

---
+++ 2.FundamentosOrientacaoObjetos/Balta/ContentContext/Lecture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Balta.ContentContext
+{
+    public class Lecture
+    {
+        public int Ordem { get; set; }
+        public string Title { get; set; }
+        
+    }
+}
