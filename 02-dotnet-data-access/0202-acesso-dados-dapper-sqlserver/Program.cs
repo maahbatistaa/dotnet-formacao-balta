8845e99ad8bb1888f788df76cf42f1ac1e99f56e
Mariana <mariana-batista99@hotmail.com>
2026-02-25 10:44:41 -0300

    feat(0202): Introducao

---
+++ 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+
+using   (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    Console.WriteLine("Conectado");
+
+    using ( var command = new SqlCommand()){
+        command.Connection = connection;
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "SELECT [Id],[Title] FROM [Category]";
+
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+        }
+    }
+}
+
