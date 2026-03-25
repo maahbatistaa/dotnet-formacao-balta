263ea536fc53ccb7a3ae9376b1f618878bb676cb
Mariana <mariana-batista99@hotmail.com>
2026-03-25 07:57:43 -0300

    feat(0202): Dapper

--- 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/BaltaDataAccess/Program.cs
+++ 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/BaltaDataAccess/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 public class Program
 {
@@ -6,27 +8,69 @@ public class Program
     {
         const string connectionString = "Server=localhost,1433;Database=balta;User Id=sa;Password=1q2w3e4r@#$; trustServerCertificate=true;";
 
+
+
         using (var connection = new SqlConnection(connectionString))
         {
-            Console.WriteLine("Conectado!");
-            connection.Open();
-
-            using (var command = new SqlCommand())
-            {
-                command.Connection = connection;
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "Select [Id], [Title] FROM [Category]";
-
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                }
-
-            }
-
+            UpdateCategory(connection);
+            ListCategories(connection);
+            // CreateCategory(connection);
         }
+    }
 
-        Console.WriteLine("Hello, World!");
+    static void ListCategories(SqlConnection connection)
+    {
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+        foreach (var item in categories)
+        {
+            Console.WriteLine($"Id: {item.Id}, Title: {item.Title}");
+        }
+    }
+
+    static void CreateCategory(SqlConnection connection)
+    {
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada para os cursos relacionados a Amazon AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSql = @"INSERT INTO [Category]
+            ([Id], [Title], [Url], [Summary], [Order], [Description], [Featured])
+        VALUES
+            (@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+
+        var rows = connection.Execute(insertSql, new
+        {
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured,
+        });
+        Console.WriteLine($"Rows: {rows}");
+    }
+
+    static void UpdateCategory(SqlConnection connection)
+    {
+        var updateQuery = @"UPDATE [Category]
+        SET
+            [Title] = @Title
+        WHERE
+            [Id] = @Id";
+
+        var rows = connection.Execute(updateQuery, new
+        {
+            Id = new Guid("daa9b6db-45bd-4ef2-9da0-c2545be469bf"),
+            Title = "Amazon AWS - Updated",
+        });
+
+        Console.WriteLine($"Rows: {rows}");
     }
 }
