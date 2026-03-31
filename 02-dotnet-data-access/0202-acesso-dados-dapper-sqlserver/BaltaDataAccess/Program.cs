32361aa5e465ad1669d98e0f6016e9deaf70d86e
Mariana <mariana-batista99@hotmail.com>
2026-03-31 18:52:50 -0300

    feat(0202): imersao

--- 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/BaltaDataAccess/Program.cs
+++ 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/BaltaDataAccess/Program.cs
@@ -15,8 +15,13 @@ public class Program
             // CreateCategory(connection);
             // UpdateCategory(connection);
             // DeleteCategory(connection);
+            // GetCategory(connection);
+            // CreateManyCategory(connection);
             // ListCategories(connection);
-            GetCategory(connection);
+            // ExecuteProcedure(connection);
+            // ExecuteReadProcedure(connection);
+            // ExecuteScalar(connection);
+            // ReadView(connection);
         }
     }
 
@@ -98,5 +103,114 @@ public class Program
         });
 
         Console.WriteLine($"{rows} registros excluídos");
+    }
+
+    static void CreateManyCategory(SqlConnection connection)
+    {
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada para os cursos relacionados a Amazon AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var category2 = new Category();
+        category2.Id = Guid.NewGuid();
+        category2.Title = "Categoria nova";
+        category2.Url = "categoria-nova";
+        category2.Description = "Categoria destinada para os cursos relacionados a Categoria nova";
+        category2.Order = 9;
+        category2.Summary = "Categoria nova";
+        category2.Featured = true;
+
+        var insertSql = @"INSERT INTO [Category]
+            ([Id], [Title], [Url], [Summary], [Order], [Description], [Featured])
+        VALUES
+            (@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+
+        var rows = connection.Execute(insertSql, new[]
+        {
+            new
+        {
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured,
+        },
+            new
+        {
+            category2.Id,
+            category2.Title,
+            category2.Url,
+            category2.Summary,
+            category2.Order,
+            category2.Description,
+            category2.Featured,
+        }});
+        Console.WriteLine($"Rows: {rows}");
+    }
+
+    static void ExecuteProcedure(SqlConnection connection)
+    {
+        var sql = "[spDeleteStudent]";
+        var pars = new { StudentId = "1b806fd0-3ce5-445f-82f0-aec2c792f1c3" };
+
+        var affectedRows = connection.Execute(sql, pars, commandType: System.Data.CommandType.StoredProcedure);
+        Console.WriteLine($"Affected rows: {affectedRows}");
+    }
+
+    static void ExecuteReadProcedure(SqlConnection connection)
+    {
+        var sql = "[spGetCoursesByCategory]";
+        var pars = new { CategoryId = "09ce0b7b-cfca-497b-92c0-3290ad9d5142" };
+
+        var courses = connection.Query(sql, pars, commandType: System.Data.CommandType.StoredProcedure);
+        foreach (var item in courses)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+
+    static void ExecuteScalar(SqlConnection connection)
+    {
+        var category = new Category();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada para os cursos relacionados a Amazon AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSql = @"INSERT INTO [Category] output inserted.Id
+        VALUES
+            (NEWID(), @Title, @Url, @Summary, @Order, @Description, @Featured) 
+        SELECT SCOPE_IDENTITY()";
+
+        var id = connection.ExecuteScalar<Guid>(insertSql, new
+        {
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured,
+        });
+        Console.WriteLine($"A category ID: {id}");
+    }
+
+    static void ReadView(SqlConnection connection)
+    {
+        var sql = "SELECT * FROM [vwCourses]";
+
+        var courses = connection.Query(sql);
+        foreach (var item in courses)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title} - {item.Category}");
+        }
     }
 }
