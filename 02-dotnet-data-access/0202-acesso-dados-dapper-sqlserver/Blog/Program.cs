96b5784673bef46348cc68a86408880cfaed7b1b
Mariana <mariana-batista99@hotmail.com>
2026-06-23 19:43:37 -0300

    feat(0202): mao na massa

--- 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/Blog/Program.cs
+++ 02-dotnet-data-access/0202-acesso-dados-dapper-sqlserver/Blog/Program.cs
@@ -5,38 +5,48 @@ using Blog.Repositories;
 
 namespace Blog
 {
-	class Program
-	{
-		private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$; trustServerCertificate=true;";
-		static void Main(string[] args)
-		{
-			var connection = new SqlConnection(CONNECTION_STRING);
-			connection.Open();
+    class Program
+    {
+        private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$; trustServerCertificate=true;";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
 
-			ReadUsers(connection);
-			ReadRoles(connection);
+            ReadUsers(connection);
+            ReadRoles(connection);
+            ReadTags(connection);
 
-			connection.Close();
-		}
+            connection.Close();
+        }
 
-		public static void ReadUsers(SqlConnection connection)
-		{
-			var repository = new Repository<User>(connection);
-			var users = repository.Get();
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var users = repository.Get();
 
-			foreach (var user in users)
-				Console.WriteLine($"Id: {user.Id}, Name: {user.Name}, Email: {user.Email}");
+            foreach (var user in users)
+                Console.WriteLine($"Id: {user.Id}, Name: {user.Name}, Email: {user.Email}");
 
-		}
+        }
 
-		public static void ReadRoles(SqlConnection connection)
-		{
-			var repository = new Repository<Role>(connection);
-			var roles = repository.Get();
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var roles = repository.Get();
 
-			foreach (var role in roles)
-				Console.WriteLine($"Id: {role.Id}, Name: {role.Name}, Slug: {role.Slug}");
+            foreach (var role in roles)
+                Console.WriteLine($"Id: {role.Id}, Name: {role.Name}, Slug: {role.Slug}");
 
-		}
-	}
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+    }
 }
