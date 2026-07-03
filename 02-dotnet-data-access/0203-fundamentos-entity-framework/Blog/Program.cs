77c1067a27aa3a779a2befba5d189f3c6059a0f3
Mariana <mariana-batista99@hotmail.com>
2026-07-03 19:05:38 -0300

    feat(0203): introdução

---
+++ 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Program.cs
@@ -0,0 +1,62 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                //var tag = new Tag { Name = ".NET", Slug = "dotnet" };
+                //context.Tags.Add(tag);
+                //context.SaveChanges();
+
+                //var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                //context.Tags.Add(tag2);
+                //context.SaveChanges();
+
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                //tag.Name = ".NET";
+                //tag.Slug = "dotnet";
+
+                //context.Update(tag);
+                //context.SaveChanges();
+
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+
+                //context.Remove(tag);
+                //context.SaveChanges();
+
+                //var tags = context
+                //    .Tags
+                //    .AsNoTracking()
+                //    .ToList();
+
+                //foreach (var tag in tags)
+                //{
+                //    Console.WriteLine(tag.Name);
+                //}
+
+                //var tag = context
+                //    .Tags
+                //    .FirstOrDefault(x => x.Id == 2);
+
+                //tag.Name = "Ponto NET";
+                //tag.Slug = "dotnet";
+
+                //context.Update(tag);
+                //context.SaveChanges();
+
+                var tag = context
+                    .Tags
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Id == 3);
+
+                Console.WriteLine(tag?.Name);
+            }
+        }
+    }
+}
