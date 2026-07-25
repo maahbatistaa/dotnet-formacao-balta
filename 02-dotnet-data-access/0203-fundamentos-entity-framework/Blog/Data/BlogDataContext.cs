4768b76fba4c7d56f1bbbea795be9ad2505f0432
Mariana <mariana-batista99@hotmail.com>
2026-07-25 14:21:11 -0300

    feat(0203): mapeamento

--- 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Data/BlogDataContext.cs
+++ 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Data/BlogDataContext.cs
@@ -8,12 +8,9 @@ namespace Blog.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
-
+      
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
-            options.LogTo(Console.WriteLine);
-        }
+            => options.UseSqlServer("Server=localhost,1433;Database=FluentBlog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
     }
 }
--- 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Models/Post.cs
+++ 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Models/Post.cs
@@ -4,11 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
-    [Table("Post")]
     public class Post
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
@@ -16,13 +13,9 @@ namespace Blog.Models
         public string Slug { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
-
-        [ForeignKey("CategoryId")]
-        public int CategoryId { get; set; }
         public Category Category { get; set; }
-
-        [ForeignKey("AuthorId")]
-        public int AuthorId { get; set; }
         public User Author { get; set; }
+
+        public List<Tag> Tags { get; set; }
     }
 }
