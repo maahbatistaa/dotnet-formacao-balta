2043193ccf84393f40e28ac73f144137c8852a67
Mariana <mariana-batista99@hotmail.com>
2026-07-25 13:44:45 -0300

    feat(0203): operacoes basicas

--- 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Data/BlogDataContext.cs
+++ 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Data/BlogDataContext.cs
@@ -8,9 +8,12 @@ namespace Blog.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
-      
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+            options.LogTo(Console.WriteLine);
+        }
     }
 }
--- 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Program.cs
+++ 02-dotnet-data-access/0203-fundamentos-entity-framework/Blog/Program.cs
@@ -11,35 +11,58 @@ namespace Blog
         {
             using var context = new BlogDataContext();
 
-            var user = new User
-            {
-                Name = "Mariana Batista",
-                Slug = "marianabatista",
-                Email = "mariana@batista.com",
-                Bio = "Dev .NET",
-                Image = "https://balta.io",
-                PasswordHash = "123098457"
-            };
+            //var user = new User
+            //{
+            //    Name = "Mariana Batista",
+            //    Slug = "marianabatista",
+            //    Email = "mariana@batista.com",
+            //    Bio = "Dev .NET",
+            //    Image = "https://balta.io",
+            //    PasswordHash = "123098457"
+            //};
 
-            var category = new Category
-            {
-                Name = "Backend",
-                Slug = "backend"
-            };
+            //var category = new Category
+            //{
+            //    Name = "Backend",
+            //    Slug = "backend"
+            //};
 
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<p>Hello world</p>",
-                Slug = "comecando-com-ef-core",
-                Summary = "Nesse artigo vamos aprender EF core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now
-            };
+            //var post = new Post
+            //{
+            //    Author = user,
+            //    Category = category,
+            //    Body = "<p>Hello world</p>",
+            //    Slug = "comecando-com-ef-core",
+            //    Summary = "Nesse artigo vamos aprender EF core",
+            //    Title = "Começando com EF Core",
+            //    CreateDate = DateTime.Now,
+            //    LastUpdateDate = DateTime.Now
+            //};
 
-            context.Posts.Add(post);
+            //context.Posts.Add(post);
+            //context.SaveChanges();
+
+            //var posts = context
+            //    .Posts
+            //    .AsNoTracking()
+            //    .Include(x => x.Author)
+            //    .Include(x => x.Category)
+            //    .OrderBy(x => x.LastUpdateDate)
+            //    .ToList();
+
+            //foreach (var post in posts)
+            //    Console.WriteLine($"{post.Title} escrito por {post.Author?.Name} em {post.Category?.Name}");
+
+            var post = context
+                .Posts
+                //.AsNoTracking() Precisa do Tracking
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .FirstOrDefault(); //Pegando o primeiro item
+
+            post.Author.Name = "Teste";
+            context.Posts.Update(post);
             context.SaveChanges();
         }
     }
